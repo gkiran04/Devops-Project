85e7d9bfe63f6c6c635b4321e9eb2418ae675a42
KIRAN <gkiran04@gmail.com>
2021-12-02 14:20:40 +0530

    main.tf updated

--- terraform-aks-k8s/main.tf
+++ terraform-aks-k8s/main.tf
@@ -10,6 +10,7 @@ terraform {
 provider "azurerm" {
     features {}
     subscription_id = "57b15192-2a1f-4673-b637-1e5693ec9c16"
+    tenant_id       = "b0d3d799-2a21-4cf1-ba30-3fb108d98466"
 }
 resource "azurerm_resource_group" "kubernetes" {
     name     = "aks-rg"

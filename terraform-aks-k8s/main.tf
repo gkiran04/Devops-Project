a8f0fb6e8e8393df957707f13da7a50475d281a9
KIRAN <gkiran04@gmail.com>
2021-12-02 13:59:28 +0530

    files updated

--- terraform-aks-k8s/main.tf
+++ terraform-aks-k8s/main.tf
@@ -1,13 +1,15 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>2.0"
+      }
     }
-  }
 }
 provider "azurerm" {
-  features {}
+    features {
+
+    }
 }
 resource "azurerm_resource_group" "kubernetes" {
     name     = "aks-rg"

98bb8543c388f4e3ff198d66a58516700208a3fa
KIRAN <gkiran04@gmail.com>
2021-12-02 14:14:10 +0530

    provider added

--- terraform-aks-k8s/variables.tf
+++ terraform-aks-k8s/variables.tf
@@ -2,7 +2,7 @@ variable "client_id" {
     default = "c3749e62-c9a9-450e-be68-e2c6610af347"
 }
 variable "client_secret" {
-    default = "370c27c8-6a0f-4e3d-af81-f7fafe118fd7"
+    default = "e1fe5e53-f824-48da-b39b-2e5461feff60"
 }
 
 variable "agent_count" {

4a155a42326aee96e924692a9aa37ee48f1c9d5c
KIRAN <gkiran04@gmail.com>
2021-12-02 16:45:01 +0530

    tf modified

--- terraform-aks-k8s/main.tf
+++ terraform-aks-k8s/main.tf
@@ -9,5 +9,8 @@ terraform {
 
 provider "azurerm" {
     features {}
+    client_id = "c3749e62-c9a9-450e-be68-e2c6610af347"
+    subscription_id = "57b15192-2a1f-4673-b637-1e5693ec9c16"
+    tenant_id = "b0d3d799-2a21-4cf1-ba30-3fb108d98466"
 }
 
